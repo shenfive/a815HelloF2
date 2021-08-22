ceb11a3182ea5afb2c1e95c5acdbcf2064ef1e89
Danny Shen <shenfive@hotmail.com>
2021-08-22 17:14:24 +0800

    FBButton

--- a815HelloF2/ViewController.swift
+++ a815HelloF2/ViewController.swift
@@ -5,13 +5,21 @@
 //  Created by 申潤五 on 2021/8/22.
 //
 
+
+// Swift // // Add this to the header of your file, e.g. in ViewController.swift import FBSDKLoginKit // Add this to the body class ViewController:UIViewController { override func viewDidLoad() { super.viewDidLoad() let loginButton = FBLoginButton() loginButton.center = view.center view.addSubview(loginButton) } }
+
+
+
+import FBSDKLoginKit
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let loginButton = FBLoginButton()
+        loginButton.center = self.view.center
+        self.view.addSubview(loginButton)
     }
 
 
