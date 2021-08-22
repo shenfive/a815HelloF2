cbc15dc9baac27f1080ad178836d28f7f7d00137
Danny Shen <shenfive@hotmail.com>
2021-08-22 17:00:29 +0800

    FB Step 5/helf

--- a815HelloF2/AppDelegate.swift
+++ a815HelloF2/AppDelegate.swift
@@ -5,7 +5,44 @@
 //  Created by 申潤五 on 2021/8/22.
 //
 
+
+
+
+
+// Swift // // AppDelegate.swift
+//import UIKit
+//import FBSDKCoreKit
+//@UIApplicationMain
+//class AppDelegate:UIResponder, UIApplicationDelegate {
+//    func application( _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? ) -> Bool {
+//        ApplicationDelegate.shared.application( application, didFinishLaunchingWithOptions: launchOptions )
+//        return true
+//
+//    }
+//    func application( _ app:UIApplication, open url:URL, options: [UIApplication.OpenURLOptionsKey :Any] = [:] ) -> Bool {
+//        ApplicationDelegate.shared.application( app, open: url, sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String, annotation: options[UIApplication.OpenURLOptionsKey.annotation] )
+//
+//    }
+//
+//}
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import UIKit
+import Firebase
+import FBSDKCoreKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +50,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        ApplicationDelegate.shared.application( application, didFinishLaunchingWithOptions: launchOptions )
         return true
     }
 
@@ -26,11 +64,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
 
+    }
+    func application( _ app:UIApplication, open url:URL, options: [UIApplication.OpenURLOptionsKey :Any] = [:] ) -> Bool {
+        ApplicationDelegate.shared.application( app, open: url, sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String, annotation: options[UIApplication.OpenURLOptionsKey.annotation] )
+        return true
+        
+    }
 
 }
 
