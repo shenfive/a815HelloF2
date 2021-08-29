80a53f5d0cf3110b6479167d7d117421fc192991
Danny Shen <shenfive@hotmail.com>
2021-08-29 10:01:46 +0800

    FB

--- a815HelloF2/ViewController.swift
+++ a815HelloF2/ViewController.swift
@@ -18,15 +18,29 @@
 import FBSDKLoginKit
 import UIKit
 
+
+// Swift override func viewDidLoad() { super.viewDidLoad() if let token = AccessToken.current, !token.isExpired { // User is logged in, do work such as go to next view controller. } }
+    
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let loginButton = FBLoginButton()
+        loginButton.permissions = ["public_profile", "email"]
         loginButton.center = self.view.center
         self.view.addSubview(loginButton)
+        if let token = AccessToken.current, !token.isExpired {
+            print("status is Login")
+        }else{
+            print("status is LogOut")
+        }
+        
+        
     }
 
 
+    // Swift // // Extend the code sample from 6a.Add Facebook Login to Your Code // Add to your viewDidLoad method: loginButton.permissions = ["public_profile", "email"]
 }
 
