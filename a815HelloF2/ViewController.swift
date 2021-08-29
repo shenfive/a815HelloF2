d960dbe0a9df661485aae06f4b10f3a156ae4ee9
Danny Shen <shenfive@hotmail.com>
2021-08-29 10:33:10 +0800

    FIN

--- a815HelloF2/ViewController.swift
+++ a815HelloF2/ViewController.swift
@@ -27,27 +27,53 @@ class ViewController: UIViewController, LoginButtonDelegate {
     }
     
     func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
-        
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print("Sign Out error:\(error.localizedDescription)")
+        }
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let loginButton = FBLoginButton()
-        loginButton.permissions = ["public_profile", "email"]
-        loginButton.delegate = self
-        loginButton.center = self.view.center
-        self.view.addSubview(loginButton)
+        
         if let token = AccessToken.current, !token.isExpired {
             print("status is Login")
+            let token = AccessToken.current
+            let credential = FacebookAuthProvider
+              .credential(withAccessToken: token!.tokenString)
+            
+            Auth.auth().signIn(with: credential) { result, error in
+                print("try signIn Firebase")
+                
+            }
         }else{
             print("status is LogOut")
         }
         
         
+        
+        Auth.auth().addStateDidChangeListener { auth, user in
+            if let user = user{
+                print("Sign in with:\(user.displayName)")
+            }else{
+                print("Sign in with:Fail")
+            }
+        }
+        
+        let loginButton = FBLoginButton()
+        loginButton.permissions = ["public_profile", "email"]
+        loginButton.delegate = self
+        loginButton.center = self.view.center
+        self.view.addSubview(loginButton)
+
+        
+        
+        
+        
     }
 
 
-    // Swift // // Extend the code sample from 6a.Add Facebook Login to Your Code // Add to your viewDidLoad method: loginButton.permissions = ["public_profile", "email"]
 }
 
