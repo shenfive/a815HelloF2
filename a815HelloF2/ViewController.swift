d15fe499507a466e52626eccfb68eb6b6917fe3e
Danny Shen <shenfive@hotmail.com>
2021-08-29 10:16:45 +0800

    FB to Firebase Auth

--- a815HelloF2/ViewController.swift
+++ a815HelloF2/ViewController.swift
@@ -6,29 +6,36 @@
 //
 
 
-// Swift // // Add this to the header of your file, e.g. in ViewController.swift import FBSDKLoginKit // Add this to the body class ViewController:UIViewController { override func viewDidLoad() { super.viewDidLoad() let loginButton = FBLoginButton() loginButton.center = view.center view.addSubview(loginButton) } }
-
-
-
-// Swift override func viewDidLoad() { super.viewDidLoad() if let token = AccessToken.current, !token.isExpired { // User is logged in, do work such as go to next view controller. } }
-    
-
-
 
 import FBSDKLoginKit
+import Firebase
 import UIKit
 
-
-// Swift override func viewDidLoad() { super.viewDidLoad() if let token = AccessToken.current, !token.isExpired { // User is logged in, do work such as go to next view controller. } }
+class ViewController: UIViewController, LoginButtonDelegate {
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        
+        let token = AccessToken.current
+        
+        let credential = FacebookAuthProvider
+          .credential(withAccessToken: token!.tokenString)
+        
+        Auth.auth().signIn(with: credential) { result, error in
+            print("try signIn Firebase")
+            
+        }
+        
+    }
     
-
-
-class ViewController: UIViewController {
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let loginButton = FBLoginButton()
         loginButton.permissions = ["public_profile", "email"]
+        loginButton.delegate = self
         loginButton.center = self.view.center
         self.view.addSubview(loginButton)
         if let token = AccessToken.current, !token.isExpired {
